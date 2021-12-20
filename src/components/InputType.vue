dae1d59ff1ad2ebadcd4011e9e41ec8502444142
yijun <sandy3068@yahoo.com.tw>
2021-12-20 15:20:11 +0800

    first commit

---
+++ src/components/InputType.vue
@@ -0,0 +1,13 @@
+<template>
+  <textarea :name="data.name" v-if="data.type === 'textarea'" :id="data.name"></textarea>
+  <select :name="data.name" :id="data.name" v-else-if="data.type === 'select'">
+    <option :value="item.value" v-for="item in data.items" :key="item.value">{{item.label}}</option>
+  </select>
+  <input :type="data.type" :id="data.name" :name="data.name" v-else>
+</template>
+
+<script>
+export default {
+  props: ['data'],
+};
+</script>
---
+++ src/views/Index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="formGroup">
+    <label :for="data.name.name">{{data.name.name}}：</label>
+    <input-type :data="data.name"></input-type>
+  </div>
+  <div class="formGroup">
+    <label :for="data.description.name">{{data.description.name}}：</label>
+    <input-type :data="data.description"></input-type>
+  </div>
+  <div class="formGroup">
+    <label :for="data.select.name">{{data.select.name}}：</label>
+    <input-type :data="data.select"></input-type>
+  </div>
+</template>
+
+<style scoped>
+  .formGroup {
+    display: flex;
+    margin-bottom: 16px;
+  }
+</style>
+
+<script>
+import inputType from '@/components/InputType.vue';
+
+export default {
+  data() {
+    return {
+      data: {
+        name: {
+          type: 'text',
+          name: '名稱',
+        },
+        description: {
+          type: 'textarea',
+          name: '說明',
+        },
+        select: {
+          type: 'select',
+          name: '選擇',
+          items: [
+            {
+              value: 1,
+              label: '選項A',
+            },
+            {
+              value: 2,
+              label: '選項B',
+            },
+          ],
+        },
+      },
+    };
+  },
+  components: {
+    inputType,
+  },
+};
+</script>
